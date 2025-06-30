da5ffcb8d4f34e0290d6ed45a57693f966e83f96
mwangaza12 <josephmwangaza1@gmail.com>
2025-06-30 10:29:52 +0300

    initial config

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express, { Application,Response } from 'express';
+import dotenv from 'dotenv';
+import cors from "cors"
+
+dotenv.config();
+
+const app: Application = express();
+
+// Basic Middleware
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//default route
+app.get('/', (req, res:Response) => {
+  res.send("Welcome to MediTime Express API Backend With Drizzle ORM and PostgreSQL");
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
+export default app;
+  
