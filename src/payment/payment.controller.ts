5748b34c9ea5b51001314c327a584f237b67b978
mwangaza12 <josephmwangaza1@gmail.com>
2025-08-06 11:57:30 +0300

    Stripe refactor

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -3,7 +3,7 @@ import { createPaymentService, deletePaymentService, getAllPaymentsService, getP
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-06-30.basil',
+  apiVersion: '2025-07-30.basil',
 });
 
 export const getAllPayments = async (req: Request, res: Response) => {
