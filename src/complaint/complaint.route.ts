f746abe42d10c9d37d1fd77341e09fd7ff7d9a56
mwangaza12 <josephmwangaza1@gmail.com>
2025-09-03 15:35:35 +0300

    refactor

--- src/complaint/complaint.route.ts
+++ src/complaint/complaint.route.ts
@@ -1,14 +1,11 @@
 import { Router } from "express";
 import {createComplaint,deleteComplaint,getAllComplaints,getComplaintById,updateComplaint,getComplaintByUserId, updateComplaintStatus, chatGptComplaint,} from "./complaint.controller";
 import { getComplaintReplies, addComplaintReply } from "../complaintReplies/complaintReplies.controller";
-
-
 import { pagination } from "../middleware/pagination";
 import { adminAuth, adminOrUserAuth, allRolesAuth, userAuth } from "../middleware/bearAuth";
 
 const complaintRouter = Router();
 
-// Complaint CRUD
 complaintRouter.get("/complaints", pagination, adminAuth, getAllComplaints);
 complaintRouter.get("/complaints/:id", adminAuth, getComplaintById);
 complaintRouter.post("/complaints", userAuth, createComplaint);
